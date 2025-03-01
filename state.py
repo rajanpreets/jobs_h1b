44a52b3ca7134de26b39270e06cc047afd08fcab
RAJAN <rajanpreet8singh@gmail.com>
2025-03-01 11:15:59 +0530

    state.py

--- state.py
+++ state.py
@@ -1,8 +1,8 @@
 from typing import List, TypedDict
 
 class GraphState(TypedDict):
-    
     question: str
     solution: str
-    online_search: bool
-    documents: List[str]
+    documents: List[str]  # Stores retrieved context from Pinecone
+
+    

f54bfa72b88c5796941b36a282f4d43a431e086a
RAJAN <rajanpreet8singh@gmail.com>
2025-03-01 11:35:07 +0530

    app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ FILE_UPLOAD_PROMPT = "Upload your Text file here"
 FILE_UPLOAD_TYPE = ".txt"
 
 # Read environment variables
-PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+PINECONE_API_KEY = 
 PINECONE_ENV = os.getenv("PINECONE_ENVIRONMENT")
 INDEX_NAME = os.getenv("PINECONE_INDEX")
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
